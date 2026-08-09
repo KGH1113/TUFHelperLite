cf8e6016492ca993d9ab597ff9e03f12a5df2e26
KGH1113 <gangguhyeon1113@gmail.com>
2026-08-09 21:13:05 +0900

    fix: make isolated Helper builds reproducible

--- scripts/tasks/build/mod.sh
+++ scripts/tasks/build/mod.sh
@@ -5,9 +5,12 @@ TASK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=../../lib/context.sh
 source "$TASK_DIR/../../lib/context.sh"
 
+configuration="${1:-Debug}"
+
 DOTNET_ROOT="$DOTNET_ROOT" DOTNET_ROOT_ARM64="$DOTNET_ROOT_ARM64" \
-  "$DOTNET_EXE" run \
-    --project "$TUFHELPER_LITE_PROJECT_ROOT/TUFHelperLite.Tests/TUFHelperLite.Tests.csproj" \
+  "$DOTNET_EXE" build "$TUFHELPER_LITE_PROJECT_ROOT/TUFHelperLite/TUFHelperLite.csproj" \
+    --configuration "$configuration" \
+    -p:OutputPath="$TUFHELPER_LITE_BUILD_OUTPUT/" \
     -p:AdofaiManaged="$ADOFAI_MANAGED" \
     -p:AdofaiMods="$ADOFAI_MODS_DIR" \
     -p:AdofaiIpcDll="$ADOFAIIPC_DLL" \
